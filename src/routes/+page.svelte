882d140a1d738d771fcc0d140648b9938c97ff02
Liempo <gonzalesalec@gmail.com>
2023-08-17 02:19:00 +0800

    Add config and other packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
-  import App from '$lib/components/App.svelte'
+	import App from '$lib/components/App.svelte';
 </script>
 
-<div>
-  <App />
+<div class="w-screen h-screen bg-teal-950">
+	<App />
 </div>
-
-<style>
-  :global(body) {
-    margin: 0;
-  }
-
-  div {
-    width: 100vw;
-    height: 100vh;
-    background: rgb(13, 19, 32);
-    background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
-  }
-</style>
